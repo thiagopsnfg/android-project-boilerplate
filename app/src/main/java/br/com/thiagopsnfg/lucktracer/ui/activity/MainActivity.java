a1c72bacb7d7530b8256a3698ed44e678ac5cafa
Thiago Pereira dos Santos <thiagopsnfg@gmail.com>
2017-03-07 17:35:39 -0300

    :tada: Start project
    Signed-off-by: Thiago Pereira dos Santos <thiagopsnfg@gmail.com>

---
+++ app/src/main/java/br/com/thiagopsnfg/lucktracer/ui/activity/MainActivity.java
@@ -0,0 +1,14 @@
+package br.com.thiagopsnfg.lucktracer.ui.activity;
+
+import android.os.Bundle;
+import br.com.thiagopsnfg.lucktracer.R;
+import br.com.thiagopsnfg.lucktracer.ui.activity.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/br/com/thiagopsnfg/lucktracer/ui/activity/base/BaseActivity.java
@@ -0,0 +1,7 @@
+package br.com.thiagopsnfg.lucktracer.ui.activity.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+
+}
